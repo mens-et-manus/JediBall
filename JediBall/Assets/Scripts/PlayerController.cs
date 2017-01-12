87070a97677e981a7323e05389dcc123d6673c4a
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2017-01-13 03:04:04 +0800

    Restart button working

--- JediBall/Assets/Scripts/PlayerController.cs
+++ JediBall/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-    void Restart()
+    public void Restart()
     {
 		UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex); 
     }
