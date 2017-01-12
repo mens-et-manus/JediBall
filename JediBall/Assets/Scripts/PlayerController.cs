60c3b095169dc45cfeb2cd0481f6f1c440f65769
Maryam Archie <marchie@mit.edu>
2017-01-12 18:39:17 -0500

    Merge with Keng Hoe

--- JediBall/Assets/Scripts/PlayerController.cs
+++ JediBall/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,10 @@ public class PlayerController : MonoBehaviour {
 
 	private bool active = false;
 
+	private bool won = false;
+
+	public GameObject Pins;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
@@ -33,6 +37,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		startText.text = "You Win!";
 		restartButton.gameObject.SetActive (true);
+		won = true;
 	}
 
 	public void gameStart()
@@ -46,14 +51,14 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate ()
 	{
 		if (active) {
-            if (rb.position.y <= -5.0)
+            if (rb.position.y <= -5.0 && !won)
             {
                 gameOverText.text = "Game Over!";
                 restartButton.gameObject.SetActive(true);
             }
 
             float moveHorizontal = Input.GetAxis ("Horizontal");
-			float moveVertical = Input.GetAxis ("Vertical");
+			float moveVertical = Input.GetAxis ("Vertical")+ 0.25f;
 
             Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
@@ -67,6 +72,13 @@ public class PlayerController : MonoBehaviour {
         {
             win();
         }
+        //if (other.gameObject.CompareTag ("Respawn")) {
+        //	gameOverText.text = "Game Over!";
+        //	restartButton.gameObject.SetActive (true);
+        //	//Restart();
+        //} else if (other.gameObject.CompareTag ("Finish")) {
+        //	win ();
+        //}
     }
 
     public void Restart()
@@ -79,6 +91,19 @@ public class PlayerController : MonoBehaviour {
 		if (GUILayout.Button ("Restart")) {
 			Restart ();
 		}
+	}
+
+	// check how many pins are not standing
+	int CheckPins(){
+		int nStand = 0;
+		//https://docs.unity3d.com/ScriptReference/Transform.html
+		foreach (Transform child in Pins.transform) {
+			//http://answers.unity3d.com/questions/1003884/how-to-check-if-an-object-is-upside-down.html
+			if (Mathf.Abs (Vector3.Dot (child.up, Vector3.up)) > 0.50f) {
+				nStand += 1;
+			}
+		}
+		return Pins.transform.childCount - nStand;
 	}
 
 }
