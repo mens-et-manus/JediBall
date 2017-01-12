d1ee492cf46f6dabdc668900a0455e5b310e6c8b
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2017-01-13 02:50:25 +0800

    Created JediBall project

---
+++ JediBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	public int count;
+
+	public Text gameOver;
+
+	public Button restart;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		gameOver.text = "";
+		restart.gameObject.SetActive (false);
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Respawn")) {
+			gameOver.text = "Game Over!";
+			restart.gameObject.SetActive (true);
+			//Restart();
+		}
+	}
+
+    void Restart()
+    {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex); 
+    }
+
+	void onGUI()
+	{
+		if (GUILayout.Button ("Restart")) {
+			Restart ();
+		}
+	}
+
+}
