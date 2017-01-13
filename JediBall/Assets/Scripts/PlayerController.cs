3b9d0ecaa72ce85f21e5171aa5dcda777397341a
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2017-01-14 02:26:34 +0800

    Compiled
    - Added kinect stuff
    - Added obstacles
    - Added space background

--- JediBall/Assets/Scripts/PlayerController.cs
+++ JediBall/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject Pins; // Pins object for scoring
+
 	public float speed;
 
 	private Rigidbody rb;
-
-	public int count;
 
 	public Text gameOverText;
 
@@ -18,7 +18,13 @@ public class PlayerController : MonoBehaviour {
 
 	public Button startButton;
 
-	private bool active = false;
+	private bool active = false; // whether ball is active or not
+
+	private bool won = false; // whether player has won
+
+	public Rigidbody kinectObject; // object controlled by kinect
+
+	private float TheForceTranslationX; // force on ball by kinect object
 
 	void Start ()
 	{
@@ -27,14 +33,33 @@ public class PlayerController : MonoBehaviour {
 		gameOverText.text = "";
 		restartButton.gameObject.SetActive (false);
 
+		TheForceTranslationX = kinectObject.GetComponent<Rigidbody> ().transform.position.x;
 	}
 
+	// count how many pins are down
+	public int CheckPins()
+	{
+		int nStand = 0;
+		//See https://docs.unity3d.com/ScriptReference/Transform.html
+		foreach (Transform child in Pins.transform) {
+			//See http://answers.unity3d.com/questions/1003884/how-to-check-if-an-object-is-upside-down.html
+			if (Mathf.Abs (Vector3.Dot (child.up, Vector3.up)) > 0.999f) {
+				nStand += 1;
+			}
+		}
+		return Pins.transform.childCount - nStand;
+	}
+
+	// transitions to win UI
 	public void win()
 	{
-		startText.text = "You Win!";
+		int nPin = CheckPins (); // count pins
+		startText.text = "Pins Down: " + nPin.ToString();
 		restartButton.gameObject.SetActive (true);
+		won = true;
 	}
 
+	// starts the game
 	public void gameStart()
 	{
 		rb.velocity = new Vector3 (0, 0, 15);
@@ -46,26 +71,42 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate ()
 	{
 		if (active) {
+			if (rb.position.y <= -5.0 && !won) // game over if ball falls below a certain height
+			{
+				gameOverText.text = "Game Over!";
+				restartButton.gameObject.SetActive(true);
+			}
 			float moveHorizontal = Input.GetAxis ("Horizontal");
 			float moveVertical = Input.GetAxis ("Vertical");
 
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			Vector3 movement = new Vector3 ((moveHorizontal + TheForceTranslationX), 0.0f, (moveVertical + 1f));
 
 			rb.AddForce (movement * speed);
 		}
-	}
 
+		if (won) { // updates the pin count after winning
+			int nPin = CheckPins ();
+			startText.text = "Pins Down: " + nPin.ToString ();
+		}
+
+		if (Input.GetKey (KeyCode.Escape)) { // quit application when ESC typed
+			Application.Quit ();
+		}
+	}
+		
 	void OnTriggerEnter(Collider other) 
 	{
-		if (other.gameObject.CompareTag ("Respawn")) {
-			gameOverText.text = "Game Over!";
-			restartButton.gameObject.SetActive (true);
-			//Restart();
-		} else if (other.gameObject.CompareTag ("Finish")) {
+//		if (other.gameObject.CompareTag ("Respawn")) {
+//			gameOverText.text = "Game Over!";
+//			restartButton.gameObject.SetActive (true);
+//			//Restart();
+//		} else 
+		if (other.gameObject.CompareTag ("Finish")) { // player wins when ball hits the end wall
 			win ();
 		}
 	}
 
+	// restarts level
     public void Restart()
     {
 		UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex); 
