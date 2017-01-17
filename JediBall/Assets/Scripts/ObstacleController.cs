5c8e1325ab41dd68ddb282228a5f86389a242308
Toshiaki Koike-Akino (MacBookPro2013) <toshi.koike.akino@gmail.com>
2017-01-17 15:32:29 -0500

    Shock effect testing

---
+++ JediBall/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+	public GameObject ShockParticle;
+
+	// 
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			Instantiate (ShockParticle, transform.position, Quaternion.Euler(-90,0,0));
+			Destroy (ShockParticle, 5f);
+		}
+	}
+
+}
