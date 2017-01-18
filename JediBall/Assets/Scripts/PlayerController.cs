ef2f5e331adb3196fcaca3b956a8a194d591b43a
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2017-01-19 03:41:02 +0800

    Added left and right arrows to GUI to indicate direction that force is pushing the ball

--- JediBall/Assets/Scripts/PlayerController.cs
+++ JediBall/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour {
 
 	public GameObject player; // for retrieving values from Muse
 
-	public float speed;
+	public float speedMultiplier;
+
+	public float startingSpeed = 15f;
 
 	private Rigidbody rb;
 
@@ -26,9 +28,13 @@ public class PlayerController : MonoBehaviour {
 
 	public bool useAlpha = true; // use alpha or beta waves
 
+	public bool alphaBetaForLR = true; // use alpha/beta waves for left and right movement
+
 	public Rigidbody kinectObject; // object controlled by kinect
 
 	private float TheForceTranslationX; // force on ball by kinect object
+
+	public Image leftArrow, rightArrow;
 
 	void Start ()
 	{
@@ -36,6 +42,8 @@ public class PlayerController : MonoBehaviour {
 		startText.text = "Welcome to JediBall!";
 		gameOverText.text = "";
 		restartButton.gameObject.SetActive (false);
+		leftArrow.enabled = false;
+		rightArrow.enabled = false;
 
 		TheForceTranslationX = kinectObject.GetComponent<Rigidbody> ().transform.position.x;
 	}
@@ -66,7 +74,7 @@ public class PlayerController : MonoBehaviour {
 	// starts the game
 	public void gameStart()
 	{
-		rb.velocity = new Vector3 (0, 0, 15);
+		rb.velocity = new Vector3 (0, 0, startingSpeed);
 		startButton.gameObject.SetActive (false);
 		startText.text = "";
 		active = true;
@@ -79,16 +87,9 @@ public class PlayerController : MonoBehaviour {
 		float alpha_forward = osc.alpha; // alpha relative value
 		float beta_forward = osc.beta; // beta relative value
 		float blink = osc.blink * 5; 
-		string inUse = " (in use)";
-		alphaText.text = useAlpha ? "Alpha: " + alpha_forward + inUse : "Alpha: " + alpha_forward;
-		betaText.text = useAlpha ? "Beta: " + beta_forward : "Beta: " + beta_forward + inUse;
+		changeAlphaBetaText (alpha_forward, beta_forward);
 		float connection = osc.conn;
-		if (connection > 2)
-			connectionText.text = "Connection Status: Good";
-		else if (connection == 2)
-			connectionText.text = "Connection Status: OK";
-		else
-			connectionText.text = "Connection Status: Bad";
+		changeConnectionText (connection);
 		if (active) {
 			if (rb.position.y <= -5.0 && !won) { // game over if ball falls below a certain height
 				gameOverText.text = "Game Over!";
@@ -96,11 +97,45 @@ public class PlayerController : MonoBehaviour {
 			}
 			float moveHorizontal = Input.GetAxis ("Horizontal");
 			float moveVertical = Input.GetAxis ("Vertical");
-			if (horizontal > 150 | horizontal < -150) { // threshold for left and right movement
-				//				horizontal = 0;
-				horizontal = horizontal / 700;
+			if (!alphaBetaForLR) {
+				if (horizontal > 150 | horizontal < -150) { // threshold for left and right movement
+					//				horizontal = 0;
+					horizontal = horizontal / 700;
+					if (horizontal > 0) {
+						leftArrow.enabled = false;
+						rightArrow.enabled = true;
+					} else {
+						leftArrow.enabled = true;
+						rightArrow.enabled = false;
+					}
+				} else {
+					horizontal = 0;
+					leftArrow.enabled = false;
+					rightArrow.enabled = false;
+				}
 			} else {
-				horizontal = 0;
+				if (horizontal > 100 && useAlpha && alpha_forward > 0.3f) {
+					horizontal = 1 * alpha_forward;
+					rightArrow.enabled = true;
+					leftArrow.enabled = false;
+				} else if (horizontal > 100 && !useAlpha && beta_forward > 0.3f) {
+					horizontal = 1 * beta_forward;
+					rightArrow.enabled = true;
+					leftArrow.enabled = false;
+				} else if (horizontal < -100 && useAlpha && alpha_forward > 0.3f) {
+					horizontal = -1 * alpha_forward;
+					rightArrow.enabled = false;
+					leftArrow.enabled = true;
+				} else if (horizontal < -100 && !useAlpha && beta_forward > 0.3f) {
+					horizontal = -1 * beta_forward;
+					rightArrow.enabled = false;
+					leftArrow.enabled = true;
+				} else {
+					horizontal = 0;
+					rightArrow.enabled = false;
+					leftArrow.enabled = false;
+				}
+					
 			}
 			float forward = 0;
 
@@ -111,10 +146,10 @@ public class PlayerController : MonoBehaviour {
 			}
 
 			Vector3 movement = new Vector3 ((moveHorizontal + TheForceTranslationX + horizontal), 0.0f, (moveVertical + forward));
-			rb.AddForce (movement * speed);
+			rb.AddForce (movement * speedMultiplier);
 		} else {
 			if (blink > 0)
-				useAlpha = !useAlpha;
+				toggleAlphaBeta();
 		}
 
 		if (won) { // updates the pin count after winning
@@ -152,4 +187,32 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	// changes connection status text
+	void changeConnectionText(float connection)
+	{
+		if (connection > 2)
+			connectionText.text = "Connection Status: Good";
+		else if (connection == 2)
+			connectionText.text = "Connection Status: OK";
+		else
+			connectionText.text = "Connection Status: Bad";
+	}
+
+	// changes alpha and beta text
+	void changeAlphaBetaText(float alpha_forward, float beta_forward)
+	{
+		string inUse = " (in use)";
+		alphaText.text = useAlpha ? "Alpha: " + alpha_forward + inUse : "Alpha: " + alpha_forward;
+		betaText.text = useAlpha ? "Beta: " + beta_forward : "Beta: " + beta_forward + inUse;
+	}
+
+	void toggleAlphaBeta()
+	{
+		useAlpha = !useAlpha;
+	}
+
+	public void toggleLRControls()
+	{
+		alphaBetaForLR = !alphaBetaForLR;
+	}
 }
