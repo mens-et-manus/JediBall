567b20a1aac15c832c879c1400d71392c3ab3370
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2017-01-20 04:46:08 +0800

    - Added random rotation about y-axis to obstacles

--- JediBall/Assets/Scripts/RandomPosition.cs
+++ JediBall/Assets/Scripts/RandomPosition.cs
@@ -7,6 +7,7 @@ public class RandomPosition : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		gameObject.transform.position = new Vector3 (Random.Range(-2,2),0.5f,Random.Range(-20,17));
+		gameObject.transform.rotation = Quaternion.Euler(0,Random.Range(0,360),0);
 	}
 	
 	// Update is called once per frame
