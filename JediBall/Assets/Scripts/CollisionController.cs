308e10567c327da5c79919b14fddb1d67550ec42
Toshiaki Koike-Akino (MacBookPro2013) <toshi.koike.akino@gmail.com>
2017-01-23 16:09:36 -0500

    Re-use RandomPosition and CollisionController for PckUp prefab

--- JediBall/Assets/Scripts/CollisionController.cs
+++ JediBall/Assets/Scripts/CollisionController.cs
@@ -21,23 +21,39 @@ public class CollisionController : MonoBehaviour {
 		source = GetComponent<AudioSource> ();
 	}
 
+	void RunParticle(Vector3 pos) {
+		if (ShockParticle != null) {
+			var particle = Instantiate (ShockParticle, pos, Quaternion.Euler (-90, 0, 0));
+			Destroy (particle, 3f);
+		}
+	}
+
+	void RunSound(float vol = 0.7f) {
+		if (ShockSound != null) {
+			// randomize audio pitch
+			float pitch = Random.Range (0.7f, 1.3f);
+			source.pitch = pitch;
+			source.PlayOneShot (ShockSound, vol);
+		}
+	}
+
 	void OnCollisionEnter(Collision other) {
 		float velocity = other.relativeVelocity.magnitude;
 		if (other.gameObject.CompareTag (TagName) && velocity > VelocityThreshold) {
+			// hit particle at contact point
+			RunParticle(other.contacts [0].point);
+			// hit sound: volume is velocity function
+			float vol = 1f - Mathf.Exp (-(velocity - VelocityThreshold) * VolumeSlope);
+			RunSound (vol);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag (TagName)) {
 			// hit particle
-			if (ShockParticle != null) {
-				var particle = Instantiate (ShockParticle, other.contacts [0].point, Quaternion.Euler (-90, 0, 0));
-				Destroy (particle, 3f);
-			}
-			// hit sound
-			if (ShockSound != null) {
-				// randomize audio pitch
-				float pitch = Random.Range (0.7f, 1.3f);
-				source.pitch = pitch;
-				// volume
-				float vol = 1f - Mathf.Exp (-(velocity - VelocityThreshold) * VolumeSlope);
-				source.PlayOneShot (ShockSound, vol);
-			}
+			RunParticle (transform.position);
+			// hit sound: volume is velocity function
+			RunSound (0.9f);
 		}
 	}
 
